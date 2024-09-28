1bb4d9b93fcc194c34b832f2603ec546403933ca
Christine Carter <pxe50zgd@students.codeinstitute.net>
2024-09-28 11:15:14 +0000

    Add excerpt field to Post model and migrate to database

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '!+8?0!-NxX8kkk$]GCzrqsA//,jkk&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-christineec-djangoblog-ijyai7bork0.ws.codeinstitute-ide.net','.herokuapp.com']
 
