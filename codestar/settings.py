21b7666905554cd2d2a8d68b7056f31b81be6c4f
Christine Carter <pxe50zgd@students.codeinstitute.net>
2024-10-01 14:29:08 +0000

    Change secret key as settings file was unsaved prior to push.

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pJ/9|Q`qk]LRCWH'
+SECRET_KEY = 'Kn0ppix3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-christineec-djangoblog-ijyai7bork0.ws.codeinstitute-ide.net','.herokuapp.com']
 
